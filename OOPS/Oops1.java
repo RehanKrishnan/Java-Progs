eff860a022f8121db664c6cb11866549b97bafe5
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-09 20:45:14 +0530

    Create Oops1.java

---
+++ OOPS/Oops1.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+class Myself {
+    int age = 27;
+    String name = "abc";
+
+    public static void main(String[] args) {
+        Myself M1 = new Myself();
+        System.out.print(M1.age);
+        System.out.println();
+        System.out.println(M1.name);
+    }
+}
