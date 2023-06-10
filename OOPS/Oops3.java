eb4f2b343741202c07487e888984e62311ebd221
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-10 12:21:38 +0530

    Create Oops3.java

---
+++ OOPS/Oops3.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+class Vehicle {
+    public void drive() {
+        System.out.println("4 Wheeler");
+    }
+}
+
+class Car extends Vehicle {
+    public void drive() {
+        System.out.println("Car - Hyundai");
+    }
+}
+
+public class Oops3 {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        vehicle.drive(); // Output: Repairing a vehicle
+        car.drive(); // Output: Repairing a car
+    }
+}
