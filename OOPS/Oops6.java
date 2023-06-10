3346bb2006175b0ea18a2baafc80cec6fe8ee232
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-10 20:25:23 +0530

    Create Oops6.java

---
+++ OOPS/Oops6.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+class Oops6 {
+    String name = "i20";
+    int year = 2018;
+
+    public static void main(String[] args) {
+        Oops6 C6 = new Oops6();
+        System.out.print("Car name: " + C6.name);
+        System.out.println();
+        System.out.println("Year of purchase: " + C6.year);
+    }
+}
