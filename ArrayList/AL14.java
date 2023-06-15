0967a4bbd7837b46f26ed66bf4945707b0310997
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-15 11:52:39 +0530

    Create AL14.java

---
+++ ArrayList/AL14.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.*;
+
+class AL14 {
+    public static void main(String args[]) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Honda");
+        cars.add("Hyundai");
+        cars.add("Toyota");
+        cars.add("Volkeswagen");
+        cars.add("Jeep");
+        cars.add("Fiat");
+        cars.add("Ford");
+        cars.clear();
+        System.out.println(cars);
+    }
+}
