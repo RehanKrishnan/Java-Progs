27ac15867caf0f501cbd871fa593a1a633709cb9
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-15 10:34:31 +0530

    Create AL1.java

---
+++ ArrayList/AL1.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+import java.util.*;
+
+class AL1 {
+    public static void main(String args[]) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Honda");
+        cars.add("Hyundai");
+        cars.add("Toyota");
+        cars.add("Volkeswagen");
+
+        System.out.println(cars);
+
+    }
+}
