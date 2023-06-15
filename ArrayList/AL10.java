3c97f6ebe99516fc49247bb8fe833b4877c2f1a5
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-15 11:37:06 +0530

    Create AL10.java

---
+++ ArrayList/AL10.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.*;
+
+class AL10 {
+    public static void main(String args[]) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Honda");
+        cars.add("Hyundai");
+        cars.add("Toyota");
+        cars.add("Volkeswagen");
+        cars.add("Jeep");
+        cars.add("Fiat");
+        cars.add("Ford");
+
+        System.out.println(cars.contains("Toyota"));
+    }
+}
