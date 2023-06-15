0e2bedc451e92400cb172579f60559ccd1ef331d
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-15 12:23:51 +0530

    Create AL16.java

---
+++ ArrayList/AL16.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.*;
+
+class AL16 {
+    public static void main(String args[]) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Honda");
+        cars.add("Hyundai");
+        cars.add("Toyota");
+        cars.add("Volkeswagen");
+        cars.add("Jeep");
+        cars.add("Fiat");
+        cars.add("Ford");
+        System.out.println(cars.indexOf("Volkeswagen"));
+    }
+}
