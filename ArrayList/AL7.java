651d2ce8cd7861ebb742005dc11788ec6189fd22
RehanKrishnan <rehankrishnan14@gmail.com>
2023-06-15 11:23:05 +0530

    Create AL7.java

---
+++ ArrayList/AL7.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.*;
+
+class AL7 {
+    public static void main(String args[]) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Honda");
+        cars.add("Hyundai");
+        cars.add("Toyota");
+        cars.add("Volkeswagen");
+
+        System.out.println(cars);
+        System.out.println(cars.size());
+
+    }
+}
